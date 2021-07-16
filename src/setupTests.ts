0a77607309c903cd0d11052111e3496ea4b94aec
vanviet.tran <vanviet.tran@gmail.com>
2021-07-16 12:00:25 +0700

    initial commit with superplate

---
+++ src/setupTests.ts
@@ -0,0 +1,28 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+
+import nock from "nock";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
+
+afterAll(() => {
+  nock.cleanAll();
+  nock.restore();
+});
+
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
+
+window.scroll = jest.fn();
+window.alert = jest.fn();
